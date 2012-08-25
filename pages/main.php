a127335d1fb0425a92aedc373ae0f9efdca66e17
Serrano Pereira <serrano.pereira@gmail.com>
2012-08-25 00:45:33 -0400

    Updated the web page title tag

--- pages/main.php
+++ pages/main.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title>MaSIS</title>
+    <title>MaSIS &mdash; Marine Species Identification System</title>
     <!-- stylesheets: -->
     <link rel="stylesheet" href="resources/openlayers/theme/default/style.css" type="text/css" />
     <link rel="stylesheet" href="resources/jquery.filetree/jquery.filetree.css" type="text/css" />
